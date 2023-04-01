5204ee91f346c74f20c2d837b8f56d58a776e3f4
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2023-03-31 21:31:58 -0700

    fixed workflow

---
+++ CMakeFiles/leptjson.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leptjson.dir/leptjson.c.o"
+  "CMakeFiles/leptjson.dir/leptjson.c.o.d"
+  "leptjson"
+  "leptjson.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/leptjson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
